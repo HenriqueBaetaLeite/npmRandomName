ae6e4f6d95ede4a49240d3a7c63d78630661927c
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2021-10-03 17:28:51 -0300

    5 nomes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const people = [
   { name: "Elon Musk" },
   { name: "Neil Gunther" },
   { name: "Antonio Neri" },
+  { name: "Jack Dorsey" },
+  { name: "Steve Jobs" },
 ];
 
 const randomName = () => "Henrique Baêta";
