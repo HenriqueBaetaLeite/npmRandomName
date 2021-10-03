e7fb68ff5bd8a999b804c83291fa0a00e224715d
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2021-10-03 17:30:41 -0300

    test if returns a string

--- tests/randomName.spec.js
+++ tests/randomName.spec.js
@@ -4,4 +4,7 @@ describe("Test if its a function", () => {
   it("its a function?", () => {
     expect(typeof randomName).toBe("function");
   });
+  it("returns a string?", () => {
+    expect(typeof randomName()).toBe("string");
+  });
 });
